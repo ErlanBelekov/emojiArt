06caf7ee6415e74b81b8169b14d6df74782091b4
erlanbelekov <belekoverlanhehe@gmail.com>
2021-03-24 10:25:23 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Erlan on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
