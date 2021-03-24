3df069bc7816e839d0d9f9782d12aedf7cb80085
erlanbelekov <belekoverlanhehe@gmail.com>
2021-03-24 13:58:35 +0100

    init ViewModel on app start

--- EmojiArt/EmojiArtApp.swift
+++ EmojiArt/EmojiArtApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct EmojiArtApp: App {
+    let artDocument = EmojiArtDocument()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiArtDocumentView(document: artDocument)
         }
     }
 }
